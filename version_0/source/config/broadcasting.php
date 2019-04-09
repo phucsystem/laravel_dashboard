422ad5fb9ec966b1ca27d6670ca44902d9521058
phuc dang <phucsystem@gmail.com>
2019-04-09 08:29:06 +0700

    Update pusher env config

--- version_0/source/config/broadcasting.php
+++ version_0/source/config/broadcasting.php
@@ -36,10 +36,18 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => '127.0.0.1',
+                'cluster' => env('PUSHER_CLUSTER'),
+            ],
+        ],
+
+        'laravel-echo-server' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_KEY'),
+            'secret' => env('PUSHER_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'host' => 'localhost',
                 'port' => 6001,
-                'scheme' => 'http'
             ],
         ],
 
