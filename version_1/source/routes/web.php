75d76c031f3273e6d798e657a96896cd43127eaf
phuc dang <phucsystem@gmail.com>
2019-04-19 11:15:24 +0700

    Update folder structure

--- version_1/source/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'auth.basic'], function () {
-    Route::get('/', 'DashboardController@index');
-});
-
-Route::post('/webhook/github', 'GitHubWebhookController@gitRepoReceivedPush');
-
-Route::ohDearWebhooks('/oh-dear-webhooks');
