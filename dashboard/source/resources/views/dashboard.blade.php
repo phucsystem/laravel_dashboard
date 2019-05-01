21ec8669ea54669e17aa6b6d9456033239f5bff6
phuc dang <phucsystem@gmail.com>
2019-05-01 14:43:30 +0700

    Update dashboard.blade.php

--- dashboard/source/resources/views/dashboard.blade.php
+++ dashboard/source/resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
     <dashboard class="font-sans">
         <uptime position="a1:a24"></uptime>
         <last-mails position="b1:b24"></last-mails>
-        <team-member name="Johan" avatar="{{ gravatar('ajohan@webbab.se') }}" birthday="1990-10-11" position="d1:d4"></team-member>
+        <team-member name="Johan" avatar="{{ gravatar('johan@webbab.se') }}" birthday="1990-10-11" position="d1:d4"></team-member>
         <team-member name="Daniel" avatar="{{ gravatar('daniel@webbab.se') }}" birthday="1990-05-12" position="d5:d8"></team-member>
         <team-member name="Monica" avatar="{{ gravatar('monica@syvmonica.se') }}" birthday="1990-05-28" position="d9:d12"></team-member>
         <time-weather position="e1:e6" date-format="ddd DD/MM" time-zone="Europe/Stockholm" weather-city="Gävle"></time-weather>
