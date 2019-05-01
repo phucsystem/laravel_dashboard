e8e24952af122731900d1febbe5a18b4cac90948
phuc dang <phucsystem@gmail.com>
2019-05-01 14:31:10 +0700

    Update dashboard.blade.php

--- dashboard/source/resources/views/dashboard.blade.php
+++ dashboard/source/resources/views/dashboard.blade.php
@@ -7,12 +7,9 @@
     <dashboard class="font-sans">
         <uptime position="a1:a24"></uptime>
         <last-mails position="b1:b24"></last-mails>
-        <team-member name="adriaan" avatar="{{ gravatar('adriaan@spatie.be') }}" birthday="1995-10-22" position="c1:b8"></team-member>
-        <team-member name="brent" avatar="{{ gravatar('brent@spatie.be') }}" birthday="1994-07-30" position="c9:b16"></team-member>
-        <team-member name="ruben" avatar="{{ gravatar('ruben@spatie.be') }}" birthday="1994-05-16" position="c17:b24"></team-member>
-        <team-member name="alex" avatar="{{ gravatar('alex@spatie.be') }}" birthday="1996-02-05" position="d1:c8"></team-member>
-        <team-member name="freek" avatar="{{ gravatar('freek@spatie.be') }}" birthday="1979-09-22" position="d9:c16"></team-member>
-        <team-member name="sebastian" display-name="seb" avatar="{{ gravatar('sebastian@spatie.be') }}" birthday="1992-02-01" position="d17:d24"></team-member>
+        <team-member name="Johan" avatar="{{ gravatar('ajohan@webbab.se') }}" birthday="1990-10-11" position="d1:d4"></team-member>
+        <team-member name="Daniel" avatar="{{ gravatar('daniel@webbab.se') }}" birthday="1990-05-12" position="d5:d8"></team-member>
+        <team-member name="Monica" avatar="{{ gravatar('monica@syvmonica.se') }}" birthday="1990-05-28" position="d9:d12"></team-member>
         <time-weather position="e1:e6" date-format="ddd DD/MM" time-zone="Europe/Stockholm" weather-city="Gävle"></time-weather>
         <internet-connection position="e1:e6"></internet-connection>
         <orders position="e7:e24"></orders>
